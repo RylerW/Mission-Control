5889e69938b7b610212021092c92b2ceb7364013
RylerW <raymond.richard1533@gmail.com>
2026-02-28 02:05:51 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+import './styles.css'
+
 const queryClient = new QueryClient()
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </QueryClientProvider>
 )
